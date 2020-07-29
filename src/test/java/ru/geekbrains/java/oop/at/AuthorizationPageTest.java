c7b6af8398d6463058f0dcc40e5a72a44732eea7
Slava62 <57189338+slava62@users.noreply.github.com>
2020-07-29 15:57:54 +0300

    Task 6 completed

--- src/test/java/ru/geekbrains/java/oop/at/AuthorizationPageTest.java
+++ src/test/java/ru/geekbrains/java/oop/at/AuthorizationPageTest.java
@@ -1,9 +1,7 @@
 package ru.geekbrains.java.oop.at;
 
-import org.hamcrest.Matcher;
-import org.hamcrest.MatcherAssert;
+
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -12,13 +10,16 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import ru.geekbrains.java.oop.at.base.BaseTest;
 import ru.geekbrains.java.oop.at.page.AuthorizationPage;
 import ru.geekbrains.java.oop.at.page.ContentPage;
-import ru.geekbrains.java.oop.at.page.SearchPage;
 
 import java.util.List;
 
 import static org.hamcrest.Matchers.*;
+import io.qameta.allure.*;
 
-@DisplayName("Проверка авторизации")
+@Epic(value = "Авторизация на главной странице GeekBrains")
+@Feature("Тест-кейс: Авторизация с валидными и не валидными данными")
+//@Story("Проверка авторизации")
+@DisplayName("Тест-кейс авторизации пользователя с валидными и не валидными данными")
 public class AuthorizationPageTest extends BaseTest {
     private ContentPage contentPage;
     private AuthorizationPage authorizationPage;
@@ -26,13 +27,27 @@ public class AuthorizationPageTest extends BaseTest {
     public static final String LOGIN = "hks47018@eoopy.com";
     public static final String PASSWORD="hks47018";
 
-    @DisplayName("Проверка авторизации с валидными данными")
+
+    @Description("Проверяем что страница \"Главная\" отображается при успешной аторизации")
+   // @Issue("BUG-100500")
+   // @TmsLink("TEST-100")
+    @DisplayName("Проверка: отображается страница \"Главная\"")
+    @Step("Логин: {this.LOGIN} пароль: {this.PASSWORD}.")
     @Test
+    @Severity(SeverityLevel.CRITICAL)
     public void authorizationValidData() {
-       chromeDriver.get(LOGIN_PAGE);
-       authorizationPage = new AuthorizationPage(chromeDriver);
-       contentPage = authorizationPage.authorization(LOGIN,PASSWORD);
-       contentPage.checkNamePage("Главная");
+        chromeDriver.get(LOGIN_PAGE);
+        authorizationPage = new AuthorizationPage(chromeDriver);
+        contentPage = authorizationPage.authorization(LOGIN, PASSWORD);
+        checkFilter();
+    }
+
+    @Step("Проверка работы фильтра курсов")
+    @Description("Проверяем что курсы \"Тестирование ПО. Уровень 1\" и" +
+            " \"Теститрование ПО. Уровень 2\" отображаются при выборе чекбоксов" +
+            " Бесплатные и Тестирование в разделе Курсы")
+    public void checkFilter() {
+
        contentPage.getButtonMenu().click();
        contentPage.getButton("Курсы").click();
        contentPage.getNavButtonCourses().click();
@@ -49,6 +64,29 @@ public class AuthorizationPageTest extends BaseTest {
         assertion("2", equalTo(2));
     }
 
+    @Description("Проверяем сообщение об ошибке при некорректном логине")
+    // @Issue("BUG-100500")
+    // @TmsLink("TEST-100")
+    @DisplayName("Проверка: остаемся на странице \"Вход\" при вводе некорректного логина")
+    @Test
+    @Step("Логин: \" \" пароль: {this.PASSWORD}.")
+    @Severity(SeverityLevel.CRITICAL)
+    public void authorizationInvalidLogin() {
+        chromeDriver.get(LOGIN_PAGE);
+        authorizationPage = new AuthorizationPage(chromeDriver);
+        contentPage = authorizationPage.authorization(" ",PASSWORD);
+        contentPage.checkNamePage("Вход");
+    }
+    @DisplayName("Проверка: остаемся на странице \"Вход\" при вводе некорректного пароля")
+    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Step("Логин: {this.LOGIN} пароль: \"password\".")
+    public void authorizationInvalidPassword() {
+        chromeDriver.get(LOGIN_PAGE);
+        authorizationPage = new AuthorizationPage(chromeDriver);
+        contentPage = authorizationPage.authorization(LOGIN,"password");
+        contentPage.checkNamePage("Вход");
+    }
     @AfterEach
     @Override
     public void afterEach() {
